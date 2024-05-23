841c484719e90d708e4af14210977c9a649e865f
Shubham Das <shubhamdas@Shubhams-MacBook-Air-2.local>
2024-05-23 09:47:23 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ export default function App() {
             element={
               <ProtectedRoute >
                <Dashboard />
-                        </ProtectedRoute>
+              </ProtectedRoute>
             }
           />
 
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -2,14 +2,18 @@ import React from 'react';
 import './homepage.css';
 import { useNavigate } from "react-router-dom";
 
-
 function HomePage() {
+   const oauthHandler=async(e)=>{
+    e.preventDefault();
+    alert("Going to use G-Oauth");
+    window.location.href = "http://localhost:3002/auth";
+   }
    const navigate = useNavigate();
    return (
     <div className='complete-page'>
       <button onClick={() => navigate('/register')}>New User!!! Register</button>
       <button onClick={() => navigate('/login')}>Login With Password</button>
-      <button onClick={() => navigate('/google-oauth')}>Login With Google</button>
+      <button onClick={oauthHandler}>Login With Google</button>
     </div>
   );
       };
